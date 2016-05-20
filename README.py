ab9edb310b21910a087994752083c1ec87acb789
Will Flowers <will.flwrs@gmail.com>
2016-05-20 13:43:58 -0400

    Delete old message

--- README.py
+++
@@ -1,8 +0,0 @@
-# Hey Clinton,
-#
-# I got this to work, but, it's nothing special. I had trouble putting random sequences of functions together.' \
-# I did find the sympy library, and I think that maybe would have helped, but was not able to implement it.
-# You should be able to see what I was going for in my code. What could I have done to make it work?
-# Of the code that worked, an image I liked was seed 883. Thanks, and see ya tomorrow.
-#
-# -Will
